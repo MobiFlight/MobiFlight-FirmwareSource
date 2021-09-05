ca321cca01316209560fb89ac59c67e2812a8b0a
elral <3263285+elral@users.noreply.github.com>
2021-09-05 08:58:33 +0200

    w/o RotaryEncoder and TickPerSecond libs

--- include/MFEncoder.h
+++ include/MFEncoder.h
@@ -5,6 +5,18 @@
 /// \version 1.0 Initial release
 /// \author  Sebastian Moebius (mobiflight@moebiuz.de) DO NOT CONTACT THE AUTHOR DIRECTLY: USE THE LISTS
 // Copyright (C) 2013-2014 Sebastian Moebius
+/// \version 1.1 functions from RotaryEncoder library included and TicksPerSecond not any more required
+/// \changes implemented from Ralf Kull
+// It is based on the RotaryEncoder library from Matthias Hertel, see below
+// -----
+// RotaryEncoder.cpp - Library for using rotary encoders.
+// This class is implemented for use with the Arduino environment.
+// Copyright (c) by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// More information on: http://www.mathertel.de/Arduino
+// -----
+// 18.01.2014 created by Matthias Hertel
+// -----
 
 #ifndef MFEncoder_h
 #define MFEncoder_h
@@ -17,23 +29,16 @@
 #include <wiring.h>
 #endif
 
-///#include "../Button/Button.h"
-///#include "../TicksPerSecond/TicksPerSecond.h"
-///#include "../RotaryEncoderAcelleration/RotaryEncoderAcelleration.h"
-#include <RotaryEncoder.h>
-
 extern "C"
 {
   typedef void (*encoderEvent) (uint8_t, uint8_t, const char *);
 };
 
 // this prevents the internal position overflow.
-// no need to change this
 #define MF_ENC_MAX 8000 
 
 // this defines the delta value limit for triggering onFast
-// this should work well for all encoder types
-#define MF_ENC_FAST_LIMIT 40
+#define MF_ENC_FAST_LIMIT 50
 
 enum
 {
@@ -43,25 +48,49 @@ enum
   encRightFast
 };
 
+typedef struct {
+	// Detent positions in the quadrature (by value, not position)
+	bool    detents[4];
+
+	// Bit shift to apply given the detent resolution of this encoder.
+	//
+	// Example: An encoder with 1 detent per quadrature cycle has a useful resolution of
+  // 1/4 of the number of pulses so we can apply a simple bit shift of 2 to 
+	// determine the effective position of the encoder.
+	uint8_t resolutionShift;
+} encoderType;
+
 /////////////////////////////////////////////////////////////////////
 /// \class MFEncoder MFEncoder.h <MFEncoder.h>
 class MFEncoder
 {
 public:
     MFEncoder();
-	  void attach(uint8_t pin1, uint8_t pin2, uint8_t encoderType, const char * name = "Encoder");
+	  void attach(uint8_t pin1, uint8_t pin2, uint8_t TypeEncoder, const char * name = "Encoder");
     void update();
     void attachHandler(uint8_t eventId, encoderEvent newHandler);
+// call this function every some milliseconds or by using an interrupt for handling state changes of the rotary encoder.
+    void tick(void);
+// retrieve the current position
+    int16_t  getPosition();
+// adjust the current position
+    void setPosition(int16_t newPosition);
     
 private:
     uint8_t                   _pin1;              
     uint8_t                   _pin2;
     bool                      _initialized;
-    RotaryEncoder             _encoder;
     const char *              _name;
-    long                      _pos;
+    int16_t                   _pos;
     encoderEvent              _handlerList[4];
-    uint8_t                   _encoderType;
-    uint16_t                  _fastLimit = MF_ENC_FAST_LIMIT;
+    uint8_t                   _TypeEncoder;
+
+    encoderType               _encoderType;
+    int8_t                    _oldState;
+    int16_t                   _position;            // Internal position (4 times _positionExt)
+    int16_t                   _positionExt;         // External position
+    uint32_t                  _lastmillis = 0;      // for debouncing
+    uint32_t                  _positionTime;        // time last position change was detected
+    uint32_t                  _positionTimePrev;    // time previous position change was detected
 };
 #endif 
--- src/MF_Modules/MFEncoder.cpp
+++ src/MF_Modules/MFEncoder.cpp
@@ -1,57 +1,106 @@
 // MFEncoder.cpp
-//
+// It is based on the RotaryEncoder library from Matthias Hertel, see below
 // Copyright (C) 2013-2014
+
+// -----
+// RotaryEncoder.cpp - Library for using rotary encoders.
+// This class is implemented for use with the Arduino environment.
+// Copyright (c) by Matthias Hertel, http://www.mathertel.de
+// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
+// More information on: http://www.mathertel.de/Arduino
+// -----
+// 18.01.2014 created by Matthias Hertel
+// -----
 
 #include "MFEncoder.h"
 
-MFEncoder::MFEncoder() : _encoder() {
+// The array holds the values -1 for the entries where a position was decremented,
+// a 1 for the entries where the position was incremented
+// and 0 in all the other (no change or not valid) cases.
+const int8_t KNOBDIR[] = {
+   0, -1,  1,  0,
+   1,  0,  0, -1,
+  -1,  0,  0,  1,
+   0,  1, -1,  0};
+
+// Configuration for different types of rotary encoders.
+// For more information, refer to http://svglobe.com/arduino/in_encoder.html
+//
+// The detents in the encoder type settings below are indexed 
+// by value, not by the order in the quadrature cycle. For example, a rotary 
+// encoder with detents at 00 and 11 (positions 0 and 2 in the 
+// quadrature) but are indexed based on their decimal values (0 and 3).
+// This allows simple lookup of detent positions by value.
+const encoderType encoderTypes[]{
+
+	// 1 detents per cycle:  00 ,  10 , [11],  01 
+	{ { false, false, false, true }, 2 },
+
+	// 1 detents per cycle: [00],  10 ,  11 ,  01 
+	{ { true, false, false, false }, 2 },
+
+	// 2 detents per cycle: [00],  10 , [11],  01 
+	{ { true, false, false, true }, 1 },
+
+	// 2 detents per cycle:  00 , [10],  11,  [01]
+	{ { false, true, true, false }, 1 },
+
+	// 4 detents per cycle: [00], [10], [11], [01]
+	{ { true, true, true, true }, 0 },
+};
+
+MFEncoder::MFEncoder() {
   _initialized = false;
 }
 
-void MFEncoder::attach(uint8_t pin1, uint8_t pin2, uint8_t encoderType, const char * name)
+void MFEncoder::attach(uint8_t pin1, uint8_t pin2, uint8_t TypeEncoder, const char * name)
 {
   _pos   = 0;  
   _name  = name;
   _pin1  = pin1;
   _pin2  = pin2;
-  _encoderType = encoderType;
-  
-  _encoder.initialize(_pin1, _pin2, _encoderType);
-  _encoder.setMinMax(-1000,1000);
-  _encoder.setPosition(_pos);
-  
+  _encoderType = encoderTypes[TypeEncoder];
+
+	pinMode(_pin1, INPUT);
+	digitalWrite(_pin1, HIGH);
+	pinMode(_pin2, INPUT);
+	digitalWrite(_pin2, HIGH);
+  // start with position 0;
+  _oldState = 0;
+  _position = 0;
+  _positionExt = 0;
+
   _initialized = true;
 }
 
 void MFEncoder::update()
 {
   if (!_initialized) return;
-  
-  //_encoder.update();
-  _encoder.tick();
-  long pos = _encoder.getPosition();
+
+//  tick();
+  int16_t pos = getPosition();
   
   if (pos == _pos) {
     // nothing happened 
     return;
   }
-  
-  long delta = pos - _pos;
-  long dir = 1;
-  if (delta<0) dir = -1;  
-  
-  long absDelta = abs(delta);
 
-  if (absDelta < _fastLimit) {
+  int16_t delta = pos - _pos;
+  bool dir = true;
+
+  if (delta<0) dir = false;
+
+  // if (abs(delta) < (MF_ENC_FAST_LIMIT * (1 + _encoderType.resolutionShift))) {   // CHECK! if resolution has to be considered!! MF_ENC_FAST_LIMIT might also to be adjusted
+  if (abs(delta) < MF_ENC_FAST_LIMIT) {
     // slow turn detected
-    if (dir==1 && _handlerList[encLeft]!= NULL) {
+    if (dir && _handlerList[encLeft]!= NULL) {
         (*_handlerList[encLeft])(encLeft, _pin1, _name);
     } else if(_handlerList[encRight]!= NULL) {
         (*_handlerList[encRight])(encRight, _pin2, _name);
     }
   } else {
     // fast turn detected
-    if (dir==1 && _handlerList[encLeftFast]!= NULL) {
+    if (dir && _handlerList[encLeftFast]!= NULL) {
         (*_handlerList[encLeftFast])(encLeftFast,  _pin1, _name);
     } else if(_handlerList[encRightFast]!= NULL) {
         (*_handlerList[encRightFast])(encRightFast, _pin2, _name);
@@ -59,12 +108,44 @@ void MFEncoder::update()
   }
   
   // protect from overflow
-  if ( (dir > 0 && (pos + delta*2) > MF_ENC_MAX) || (dir < 0 && (pos - delta*2) < -MF_ENC_MAX)) 
+  if ( (dir  && (pos + delta*2) > MF_ENC_MAX) || (!dir && (pos - delta*2) < -MF_ENC_MAX))
   { 
-    _encoder.setPosition(0); 
+    setPosition(0); 
     pos = 0; 
   }
   _pos = pos;
+}
+
+void MFEncoder::tick(void)
+{
+	if (millis() - _lastmillis < 5) return;     // debouncing, do not read to fast
+	_lastmillis = millis();
+
+	bool sig1 = digitalRead(_pin1);
+	bool sig2 = digitalRead(_pin2);
+	
+	int8_t thisState = sig1 | (sig2 << 1);
+  
+	if (_oldState != thisState) {
+		int _speed = 1 + (1000 / (1 + _positionTime - _positionTimePrev));
+		_position += KNOBDIR[thisState | (_oldState<<2)] * _speed;
+		if (_encoderType.detents[thisState]) {
+			_positionTimePrev = _positionTime;
+			_positionTime = millis();
+			_positionExt = _position >> _encoderType.resolutionShift;
+		}
+		_oldState = thisState;
+	}
+}
+
+int16_t  MFEncoder::getPosition() {
+  return _positionExt;
+}
+
+void MFEncoder::setPosition(int16_t newPosition) {
+  // only adjust the external part of the position.
+  _position = ((newPosition >> _encoderType.resolutionShift) | (_position & 0x03));
+  _positionExt = newPosition;
 }
 
 void MFEncoder::attachHandler(uint8_t eventId, encoderEvent newHandler)
