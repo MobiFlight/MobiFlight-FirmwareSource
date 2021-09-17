7ebf23bf6af308d308f6d63dbc2ab6c2a732015f
Neil Enns <info@danecreekphotography.com>
2021-09-17 06:35:04 -0700

    Pull version number from GitHub

--- firmware_name.py
+++ firmware_name.py
@@ -1,8 +1,17 @@
 Import("env")
+import os
 
-# my_flags = env.ParseFlags(env['BUILD_FLAGS'])
-# defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
-# print(defines)
-# env.Replace(PROGNAME="firmware_%s" % defines.get("VERSION"))
+# Get the version number from the build environment. If no version
+# number is supplied from the build system then use "dev" as the
+# version as this is a local development build.
+firmware_version = os.environ.get('VERSION', 'dev')
 
-env.Replace(PROGNAME=env.GetProjectOption("firmware_name") + "_" + env.GetProjectOption("firmware_version"))
+# Github versions are formatted like this: v1.0.0. To use in the filename
+# and keep consistent with pre-PlatformIO file names strip out the v
+# and convert the periods to underscores.
+firmware_version = firmware_version.replace("v", "").replace(".", "_")
+
+print(f'Using version {firmware_version} for the build')
+
+# Set the output filename to the name of the board and the version
+env.Replace(PROGNAME=f'mobiflight_{env["PIOENV"]}_{firmware_version}')
