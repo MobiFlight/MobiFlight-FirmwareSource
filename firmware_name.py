516a75ad855b6fbc3ba496c2be0d01f1e4e3a9b4
Neil Enns <info@danecreekphotography.com>
2021-09-17 06:43:34 -0700

    Try fixing version check from github

--- firmware_name.py
+++ firmware_name.py
@@ -4,12 +4,17 @@ import os
 # Get the version number from the build environment. If no version
 # number is supplied from the build system then use "dev" as the
 # version as this is a local development build.
-firmware_version = os.environ.get('VERSION', 'dev')
+firmware_version = os.environ.get('VERSION', "")
 
-# Github versions are formatted like this: v1.0.0. To use in the filename
-# and keep consistent with pre-PlatformIO file names strip out the v
-# and convert the periods to underscores.
-firmware_version = firmware_version.replace("v", "").replace(".", "_")
+print(firmware_version)
+
+if firmware_version == "":
+  firmware_version = "dev"
+else:
+  # Github versions are formatted like this: v1.0.0. To use in the filename
+  # and keep consistent with pre-PlatformIO file names strip out the v
+  # and convert the periods to underscores.
+  firmware_version = firmware_version.replace("v", "").replace(".", "_")
 
 print(f'Using version {firmware_version} for the build')
 
