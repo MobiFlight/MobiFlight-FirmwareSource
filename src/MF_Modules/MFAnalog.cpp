06f8bf6c282ef3678887c9c8eecc272c9b80a4fd
elral <3263285+elral@users.noreply.github.com>
2021-10-21 06:47:36 +0200

    unintended changes, back to original

--- src/MF_Modules/MFAnalog.cpp
+++ src/MF_Modules/MFAnalog.cpp
@@ -10,6 +10,7 @@ MFAnalog::MFAnalog(uint8_t pin, analogEvent callback, const char * name, uint8_t
   _pin  = pin;
   _name = name;
   _lastValue = 0;
+  _last = millis();
   _handler = callback; 
   pinMode(_pin, INPUT_PULLUP);     // set pin to input. Could use OUTPUT for analog, but shows the intention :-)
   analogRead(_pin); // turn on pullup resistors
@@ -19,8 +20,9 @@ void MFAnalog::update()
 {    
     uint32_t now = millis();
     if (now-_last <= 50) return; // Analog is too spammy on the protocol to MF otherwise.
-    
+
     int newValue = (int) analogRead(_pin);
+    _last = now;
     if (abs(newValue - _lastValue) >= _sensitivity) {
       _lastValue = newValue;
        if (_handler!= NULL) {
