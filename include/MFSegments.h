8c467b26e65eac194f42011c09584b09e06a5447
elral <3263285+elral@users.noreply.github.com>
2021-10-24 16:33:21 +0200

    use modified ledcontrol lib

--- include/MFSegments.h
+++ include/MFSegments.h
@@ -3,36 +3,10 @@
 /// \mainpage MF Button module for MobiFlight Framework
 /// \par Revision History
 /// \version 1.0 Initial release
-/// \version 1.1 Required functions from LedControl library are included
 /// \author  Sebastian Moebius (mobiflight@moebiuz.de) DO NOT CONTACT THE AUTHOR DIRECTLY: USE THE LISTS
 // Copyright (C) 2013-2014 Sebastian Moebius
 
-/*
-The functions to control the MAX7219/7221 are copied from
-LedControl.h - A library for controling Leds with a MAX7219/MAX7221
-Copyright (c) 2007-2015 Eberhard Fahle
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-This permission notice shall be included in all copies or 
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
+//#define USE_POINTER
 
 #ifndef MFSegments_h
 #define MFSegments_h
@@ -44,87 +18,8 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include <wiring.h>
 #endif
 
-// #include <LedControl.h>
-#include <avr/pgmspace.h>
+#include "LedControl.h"
 
-//the opcodes for the MAX7221 and MAX7219
-#define OP_NOOP   0
-#define OP_DIGIT0 1
-#define OP_DIGIT1 2
-#define OP_DIGIT2 3
-#define OP_DIGIT3 4
-#define OP_DIGIT4 5
-#define OP_DIGIT5 6
-#define OP_DIGIT6 7
-#define OP_DIGIT7 8
-#define OP_DECODEMODE  9
-#define OP_INTENSITY   10
-#define OP_SCANLIMIT   11
-#define OP_SHUTDOWN    12
-#define OP_DISPLAYTEST 15
-
-/*
- * Segments to be switched on for characters and digits on
- * 7-Segment Displays
- * B01234567  
- *
- *   1
- * 6   2
- *   7
- * 5   3
- *   4
- */
-
-const static byte charTable[128] PROGMEM = {
-    B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,
-    B01111111,B01111011,B01110111,B00011111,B00001101,B00111101,B01001111,B01000111,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
- // (space)  ,!        ,"        ,#        ,$        ,%        ,&        ,'
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
- // (        ,)        ,*        ,+        ,,        ,-        ,.        ,/
-    B00000000,B00000000,B00000000,B00000000,B10000000,B00000001,B10000000,B00100101,
- // 0        ,1        ,2        ,3        ,4        ,5        ,6        ,7
-    B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,
- // 8        ,9        ,:        ,;        ,<        ,=        ,>        ,?
-    B01111111,B01111011,B00000000,B00000000,B00000000,B00001001,B00000000,B00000000,
- // @        ,A        ,B        ,C        ,D        ,E        ,F        ,G  
-    B00000000,B01110111,B01111111,B01001110,B01111110,B01001111,B01000111,B01011111,
- // H        ,I        ,J        ,K        ,L        ,M        ,N        ,O
-    B00110111,B00110000,B01111100,B00000000,B00001110,B00000000,B01110110,B01111110,
- // P        ,Q        ,R        ,S        ,T        ,U        ,V        ,W
-    B01100111,B01111110,B01110111,B01011011,B01000110,B00111110,B00000000,B00000000,
- // X        ,Y        ,Z        ,[        ,\        ,]        ,^        ,_
-    B00000000,B00110011,B01101101,B01001110,B00010011,B01111000,B00000000,B00001000,
- // `        ,a        ,b        ,c        ,d        ,e        ,f        ,g
-    B00000000,B01111101,B00011111,B00001101,B00111101,B01001111,B01000111,B01111011,
- // h        ,i        ,j        ,k        ,l        ,m        ,n        ,o
-    B00110111,B00110000,B00111000,B00000000,B00000110,B00000000,B00010101,B00011101,
- // p        ,q        ,r        ,s        ,t        ,u        ,v        ,w
-    B01100111,B01110011,B00000101,B01011011,B00001111,B00011100,B00000000,B00000000,
- // x        ,y        ,z        ,{        ,|        ,}        ,~        ,(delete)
-    B00000000,B00100111,B01101101,B01001110,B00000000,B01111000,B00000000,B00000000
-};
-/*
-const static byte charTable [] PROGMEM  = {
-    B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,
-    B01111111,B01111011,B01110111,B00011111,B00001101,B00111101,B01001111,B01000111,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B00000000,B00000000,B00000000,B10000000,B00000001,B10000000,B00000000,
-    B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,
-    B01111111,B01111011,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B01110111,B00011111,B00001101,B00111101,B01001111,B01000111,B00000000,
-    B00110111,B00000000,B00000000,B00000000,B00001110,B00000000,B00000000,B00000000,
-    B01100111,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00001000,
-    B00000000,B01110111,B00011111,B00001101,B00111101,B01001111,B01000111,B00000000,
-    B00110111,B00000000,B00000000,B00000000,B00001110,B00000000,B00010101,B00011101,
-    B01100111,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000
-};
-*/
 /////////////////////////////////////////////////////////////////////
 /// \class MFSegments MFSegments.h <MFSegments.h>
 class MFSegments
@@ -132,33 +27,19 @@ class MFSegments
 public:
     MFSegments();
     void display(byte module, char *string, byte points, byte mask, bool convertPoints = false);
-    void attach(int dataPin, int csPin, int clkPin, byte moduleCount, byte brightness);
+    void attach(int dataPin, int csPin, int clkPin, int moduleCount, byte brightness);
     void detach();
     void test();
     void powerSavingMode(bool state);
     void setBrightness(byte module, byte value);
 
 private:
-    void shutdown(int addr, bool b);
-    void setScanLimit(int addr, int limit);
-    void clearDisplay(int addr);
-    void setIntensity(int addr, int intensity);
-    void setDigit(int addr, int digit, byte value, boolean dp);
-    void setChar(int addr, int digit, char value, boolean dp);
-    void spiTransfer(int addr, volatile byte opcode, volatile byte data);
-//    LedControl *_ledControl;
+#ifdef USE_POINTER
+    LedControl *_ledControl;
+#else
+    LedControl _ledControl;
+#endif
     bool _initialized;
     byte _moduleCount;
-    /* The array for shifting the data to the devices */
-    byte spidata[16];
-    /* Data is shifted out of this pin*/
-    int SPI_MOSI;
-    /* The clock is signaled on this pin */
-    int SPI_CLK;
-    /* This one is driven LOW for chip selectzion */
-    int SPI_CS;
-    /* The maximum number of devices we use */
-    int maxDevices;
 };
 #endif
-
--- src/MF_Modules/MFSegments.cpp
+++ src/MF_Modules/MFSegments.cpp
@@ -1,36 +1,8 @@
 // MFSegments.cpp
 //
-// Copyright (C) 2013-2021
-//
-/*
-The functions to control the MAX7219/7221 are copied from
-LedControl.h - A library for controling Leds with a MAX7219/MAX7221
-Copyright (c) 2007-2015 Eberhard Fahle
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
-
-This permission notice shall be included in all copies or 
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
+// Copyright (C) 2013-2014
 
 #include "MFSegments.h"
-#include <avr/pgmspace.h>
 
 MFSegments::MFSegments()
 {
@@ -42,6 +14,7 @@ void MFSegments::display(byte module, char *string, byte points, byte mask, bool
   if (!_initialized)
     return;
 
+  String str = String(string);
   byte digit = 8;
   byte pos = 0;
   for (int i = 0; i != 8; i++)
@@ -49,7 +22,11 @@ void MFSegments::display(byte module, char *string, byte points, byte mask, bool
     digit--;
     if (((1 << digit) & mask) == 0)
       continue;
-    setChar(module, digit, string[pos], ((1 << digit) & points));
+#ifdef USE_POINTER
+    _ledControl->setChar(module, digit, str.charAt(pos), ((1 << digit) & points));
+#else
+    _ledControl.setChar(module, digit, str.charAt(pos), ((1 << digit) & points));
+#endif
     pos++;
   }
 }
@@ -60,47 +37,44 @@ void MFSegments::setBrightness(byte module, byte value)
     return;
   if (module < _moduleCount)
   {
-    setIntensity(module, value);
+#ifdef USE_POINTER
+    _ledControl->setIntensity(module, value);
+#else
+    _ledControl.setIntensity(module, value);
+#endif
   }
 }
 
-void MFSegments::attach(int dataPin, int csPin, int clkPin, byte moduleCount, byte brightness)
+void MFSegments::attach(int dataPin, int csPin, int clkPin, int moduleCount, byte brightness)
 {
+#ifdef USE_POINTER
+  _ledControl = new LedControl();
+  _ledControl->begin(dataPin, clkPin, csPin, moduleCount);
+#else
+  _ledControl.begin(dataPin, clkPin, csPin, moduleCount);
+#endif
   _initialized = true;
   _moduleCount = moduleCount;
-
-  SPI_MOSI=dataPin;
-  SPI_CLK=clkPin;
-  SPI_CS=csPin;
-  if(_moduleCount<=0 || _moduleCount>8 )
-      _moduleCount=8;
-  maxDevices=_moduleCount;
-  pinMode(SPI_MOSI,OUTPUT);
-  pinMode(SPI_CLK,OUTPUT);
-  pinMode(SPI_CS,OUTPUT);
-  digitalWrite(SPI_CS,HIGH);
-  SPI_MOSI=dataPin;
-  for(int i=0;i<maxDevices;i++) {
-      spiTransfer(i,OP_DISPLAYTEST,0);
-      //scanlimit is set to max on startup
-      setScanLimit(i,7);
-      //decode is done in source
-      spiTransfer(i,OP_DECODEMODE,0);
-      clearDisplay(i);
-      //we go into shutdown-mode on startup
-      shutdown(i,true);
-  }
-
   for (int i = 0; i != _moduleCount; ++i)
   {
     setBrightness(i, brightness);
-    shutdown(i, false);
-    clearDisplay(i);
+#ifdef USE_POINTER
+    _ledControl->shutdown(i, false);
+    _ledControl->clearDisplay(i);
+#else
+    _ledControl.shutdown(i, false);
+    _ledControl.clearDisplay(i);
+#endif
   }
 }
 
 void MFSegments::detach()
 {
+  if (!_initialized)
+    return;
+#ifdef USE_POINTER
+  delete _ledControl;
+#endif
   _initialized = false;
 }
 
@@ -108,7 +82,11 @@ void MFSegments::powerSavingMode(bool state)
 {
   for (byte i = 0; i != _moduleCount; ++i)
   {
-    shutdown(i, state);
+#ifdef USE_POINTER
+    _ledControl->shutdown(i, state);
+#else
+    _ledControl.shutdown(i, state);
+#endif
   }
 }
 
@@ -124,7 +102,11 @@ void MFSegments::test()
   {
     for (module = 0; module != _moduleCount; ++module)
     {
-      setDigit(module, digit, 8, 1);
+#ifdef USE_POINTER
+      _ledControl->setDigit(module, digit, 8, 1);
+#else
+      _ledControl.setDigit(module, digit, 8, 1);
+#endif
     }
     delay(_delay);
   }
@@ -133,7 +115,11 @@ void MFSegments::test()
   {
     for (module = 0; module != _moduleCount; ++module)
     {
-      setChar(module, 7 - digit, '-', false);
+#ifdef USE_POINTER
+      _ledControl->setChar(module, 7 - digit, '-', false);
+#else
+      _ledControl.setChar(module, 7 - digit, '-', false);
+#endif
     }
     delay(_delay);
   }
@@ -142,90 +128,12 @@ void MFSegments::test()
   {
     for (module = 0; module != _moduleCount; ++module)
     {
-      setChar(module, 7 - digit, ' ', false);
+#ifdef USE_POINTER
+      _ledControl->setChar(module, 7 - digit, ' ', false);
+#else
+      _ledControl.setChar(module, 7 - digit, ' ', false);
+#endif
     }
     delay(_delay);
   }
-}
-
-// --------------------------------------------------------------------------
-void MFSegments::shutdown(int addr, bool b) {
-    if(addr<0 || addr>=maxDevices)
-        return;
-    if(b)
-        spiTransfer(addr, OP_SHUTDOWN,0);
-    else
-        spiTransfer(addr, OP_SHUTDOWN,1);
-}
-
-void MFSegments::setScanLimit(int addr, int limit) {
-    if(addr<0 || addr>=maxDevices)
-        return;
-    if(limit>=0 && limit<8)
-        spiTransfer(addr, OP_SCANLIMIT,limit);
-}
-
-void MFSegments::clearDisplay(int addr) {
-    if(addr<0 || addr>=maxDevices)
-        return;
-    for(int i=0;i<8;i++) {
-        spiTransfer(addr, i+1,0);
-    }
-}
-
-void MFSegments::setIntensity(int addr, int intensity) {
-    if(addr<0 || addr>=maxDevices)
-        return;
-    if(intensity>=0 && intensity<16)	
-        spiTransfer(addr, OP_INTENSITY,intensity);
-}
-
-void MFSegments::setDigit(int addr, int digit, byte value, boolean dp) {
-    byte v;
-
-    if(addr<0 || addr>=maxDevices)
-        return;
-    if(digit<0 || digit>7 || value>15)
-        return;
-    v=pgm_read_byte_near(charTable + value); 
-    if(dp)
-        v|=B10000000;
-    spiTransfer(addr, digit+1,v);
-}
-
-void MFSegments::setChar(int addr, int digit, char value, boolean dp) {
-    byte index,v;
-
-    if(addr<0 || addr>=maxDevices)
-        return;
-    if(digit<0 || digit>7)
-        return;
-    index=(byte)value;
-    if(index >127) {
-        //no defined beyond index 127, so we use the space char
-        index=32;
-    }
-    v=pgm_read_byte_near(charTable + index); 
-    if(dp)
-        v|=B10000000;
-    spiTransfer(addr, digit+1,v);
-}
-
-void MFSegments::spiTransfer(int addr, volatile byte opcode, volatile byte data) {
-    //Create an array with the data to shift out
-    int offset=addr*2;
-    int maxbytes=maxDevices*2;
-
-    for(int i=0;i<maxbytes;i++)
-        spidata[i]=(byte)0;
-    //put our device data into the array
-    spidata[offset+1]=opcode;
-    spidata[offset]=data;
-    //enable the line 
-    digitalWrite(SPI_CS,LOW);
-    //Now shift out the data 
-    for(int i=maxbytes;i>0;i--)
-        shiftOut(SPI_MOSI,SPI_CLK,MSBFIRST,spidata[i-1]);
-    //latch the data onto the display
-    digitalWrite(SPI_CS,HIGH);
 }
