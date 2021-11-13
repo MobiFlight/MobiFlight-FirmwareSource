c3b41a05340a258125578950640a28cc5423bf02
elral <3263285+elral@users.noreply.github.com>
2021-11-13 15:28:26 +0100

    Fix for 40 char LCDs , Fixes #85

--- src/MF_Modules/MFLCDDisplay.cpp
+++ src/MF_Modules/MFLCDDisplay.cpp
@@ -13,12 +13,13 @@ void MFLCDDisplay::display(const char *string)
 {
   if (!_initialized)
     return;
-  char readBuffer[21] = "";
-  for (byte l = 0; l != _lines; l++)
+  for (uint8_t line = 0; line != _lines; line++)
   {
-    _lcdDisplay->setCursor(0, l);
-    memcpy(readBuffer, string + _cols * l, _cols);
-    _lcdDisplay->print(readBuffer);
+    _lcdDisplay->setCursor(0, line);
+    for (uint8_t col = 0; col < _cols; col++)
+    {
+      _lcdDisplay->write(string[(_cols * line) + col]);
+    }
   }
 }
 
