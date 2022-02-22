383306d7c29bcaa43c133d327cfdba791fdb63f7
Neil Enns <{ID}+{username}@users.noreply.github.com>
2022-02-22 12:42:59 -0800

    Remove whitespace changes

--- src/MF_Modules/MFOutputShifter.cpp
+++ src/MF_Modules/MFOutputShifter.cpp
@@ -16,7 +16,6 @@ void MFOutputShifter::setPin(uint8_t pin, uint8_t value, uint8_t refresh)
 
   uint8_t idx = (pin & 0xF8) >> 3;
   uint8_t msk = (0x01 << (pin & 0x07));
-
   if (value > 0)
   {
     _outputBuffer[idx] |= msk;
