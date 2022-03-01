df712e5cdc84e82ff00b037f29903d3ad8c0f078
Neil Enns <neile@live.com>
2022-03-01 10:36:51 -0800

    Increase device limits on the Uno (#162)
    Fixes #161

--- _Boards/Atmel/Board_Uno/MFBoards.h
+++ _Boards/Atmel/Board_Uno/MFBoards.h
@@ -24,14 +24,14 @@
 #endif
 
 //#define MODULE_MAX_PINS 13
-#define MAX_OUTPUTS 8
-#define MAX_BUTTONS 8
+#define MAX_OUTPUTS 18
+#define MAX_BUTTONS 18
 #define MAX_LEDSEGMENTS 1
 #define MAX_ENCODERS 3
 #define MAX_STEPPERS 2
 #define MAX_MFSERVOS 2
 #define MAX_MFLCD_I2C 2
-#define MAX_ANALOG_INPUTS 3
+#define MAX_ANALOG_INPUTS 6
 #define MAX_OUTPUT_SHIFTERS 2
 #define MAX_INPUT_SHIFTERS 2
 
