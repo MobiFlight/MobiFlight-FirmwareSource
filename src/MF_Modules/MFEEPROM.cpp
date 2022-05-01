ddc84e9078d8cec11840af199d5fc90caa1562f0
Sebastian M <86157512+DocMoebiuz@users.noreply.github.com>
2022-05-01 20:12:27 +0200

    Revert "Improvement of pointer handling in EEPROM functions for config parsing (#173)"
    This reverts commit e9f6b74526490b54d768a9479dd244278f17d125.

--- src/MF_Modules/MFEEPROM.cpp
+++ src/MF_Modules/MFEEPROM.cpp
@@ -4,55 +4,45 @@
 // (C) MobiFlight Project 2022
 //
 
-// WARNING: This code is based on Arduino / Atmel AVR libraries (although itself not AVR-specific).
-// When compiling for other platforms, check that the available libraries are compatible.
-
 #include <Arduino.h>
 #include "MFEEPROM.h"
 #include <EEPROM.h>
 
-MFEEPROM::MFEEPROM() {}
+MFEEPROM::MFEEPROM()
+{
+    eepromLength = EEPROM.length();
+}
 
 uint16_t MFEEPROM::get_length(void)
 {
-    return EEPROM.length();
+    return eepromLength;
 }
 
-bool MFEEPROM::read_block(uint16_t adr, char data[], uint16_t len)
+void MFEEPROM::read_block(uint16_t adr, char data[], uint16_t len)
 {
-    // If length is exceeded, return only the legitimate part
-    for (uint16_t i = 0; i < len && adr < EEPROM.length(); i++, adr++) {
-        data[i] = read_char(adr);
+    for (uint16_t i = 0; i < len; i++) {
+        data[i] = read_char(adr + i);
     }
-    return (adr < EEPROM.length());
 }
 
-bool MFEEPROM::write_block(uint16_t adr, char data[], uint16_t len)
+void MFEEPROM::write_block(uint16_t adr, char data[], uint16_t len)
 {
-    // If length is exceeded, do not write anything
-    if (adr + len >= EEPROM.length()) return false;
-    for (uint16_t i = 0; i < len; i++, adr++) {
-        EEPROM.put(adr, data[i]);
+    if (adr + len >= eepromLength) return;
+    for (uint16_t i = 0; i < len; i++) {
+        EEPROM.put(adr + i, data[i]);
     }
-    return true;
 }
 
 char MFEEPROM::read_char(uint16_t adr)
 {
-    if (adr >= EEPROM.length()) return 0;
+    if (adr >= eepromLength) return 0;
     return EEPROM.read(adr);
 }
 
-bool MFEEPROM::write_byte(uint16_t adr, char data)
+void MFEEPROM::write_byte(uint16_t adr, char data)
 {
-    if (adr >= EEPROM.length()) return false;
+    if (adr >= eepromLength) return;
     EEPROM.put(adr, data);
-    return true;
-}
-
-bool MFEEPROM::setPosition(uint16_t pos)
-{
-    return ((pos < EEPROM.length()) ? ((_pos = pos), true) : false);
 }
 
 // MFEEPROM.cpp
