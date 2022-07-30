a794eb142fee2a61e7cf33cf2b27e56ab9635ff2
elral <3263285+elral@users.noreply.github.com>
2022-07-30 07:36:05 +0200

    move if condition before loop in read_block

--- src/MF_Modules/MFEEPROM.cpp
+++ src/MF_Modules/MFEEPROM.cpp
@@ -22,8 +22,8 @@ uint16_t MFEEPROM::get_length(void)
 
 bool MFEEPROM::read_block(uint16_t adr, char data[], uint16_t len)
 {
+    if (adr + len > _eepromLength) return false;
     for (uint16_t i = 0; i < len; i++) {
-        if (adr + len >= _eepromLength) return false;
         data[i] = read_char(adr + i);
     }
     return true;
