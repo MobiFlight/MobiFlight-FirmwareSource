018791b30785005b750bd848398611d65b727433
Neil Enns <neile@live.com>
2023-09-05 12:20:49 -0700

    Wrap defines (#250)

--- _Boards/Atmel/Board_Nano/MFBoards.h
+++ _Boards/Atmel/Board_Nano/MFBoards.h
@@ -36,30 +36,65 @@
 #define MF_DIGIN_MUX_SUPPORT 1
 #endif
 
+#ifndef MAX_OUTPUTS
 #define MAX_OUTPUTS         18
+#endif
+#ifndef MAX_BUTTONS
 #define MAX_BUTTONS         18
+#endif
+#ifndef MAX_LEDSEGMENTS
 #define MAX_LEDSEGMENTS     1
+#endif
+#ifndef MAX_ENCODERS
 #define MAX_ENCODERS        9
+#endif
+#ifndef MAX_STEPPERS
 #define MAX_STEPPERS        2
+#endif
+#ifndef MAX_MFSERVOS
 #define MAX_MFSERVOS        2
+#endif
+#ifndef MAX_MFLCD_I2C
 #define MAX_MFLCD_I2C       2
+#endif
+#ifndef MAX_ANALOG_INPUTS
 #define MAX_ANALOG_INPUTS   8
+#endif
+#ifndef MAX_OUTPUT_SHIFTERS
 #define MAX_OUTPUT_SHIFTERS 2
+#endif
+#ifndef MAX_INPUT_SHIFTERS
 #define MAX_INPUT_SHIFTERS  2
+#endif
+#ifndef MAX_DIGIN_MUX
 #define MAX_DIGIN_MUX       3
+#endif
 
 #define STEPS         64
 #define STEPPER_SPEED 400 // 300 already worked, 467, too?
 #define STEPPER_ACCEL 800
 
+#ifndef MOBIFLIGHT_TYPE
 #define MOBIFLIGHT_TYPE     "MobiFlight Nano"
+#endif
+#ifndef MOBIFLIGHT_SERIAL
 #define MOBIFLIGHT_SERIAL   "0987654321"
+#endif
+#ifndef MOBIFLIGHT_NAME
 #define MOBIFLIGHT_NAME     "MobiFlight Nano"
+#endif
+#ifndef EEPROM_SIZE
 #define EEPROM_SIZE         1024 // EEPROMSizeUno
+#endif
+#ifndef MEMLEN_CONFIG
 #define MEMLEN_CONFIG       286  // max. size for config which wil be stored in EEPROM
+#endif
+#ifndef MEMLEN_NAMES_BUFFER
 #define MEMLEN_NAMES_BUFFER 220  // max. size for configBuffer, contains only names from inputs
+#endif
+#ifndef MF_MAX_DEVICEMEM
 #define MF_MAX_DEVICEMEM    300  // max. memory size for devices
-
+#endif
 
 #endif
 
