f20f703111f8a30c750a1feca353f6634e5fce9c
elral <3263285+elral@users.noreply.github.com>
2023-09-21 06:29:17 +0200

    don't use backslash in file path from script

--- get_CustomDevices.py
+++ get_CustomDevices.py
@@ -9,4 +9,4 @@ if not os.path.exists(CUSTOMDEVICES_DIR):
     env.Execute("git clone --depth 100 https://github.com/elral/MobiFlight-CustomDevices $PROJECT_DIR/CustomDevices")
 else:
     print ("Checking for Mobiflight-CustomDevices repo updates ... ")
-    env.Execute("git --work-tree=$PROJECT_DIR\CustomDevices --git-dir=$PROJECT_DIR\CustomDevices\.git pull origin main --depth 100")
+    env.Execute("git --work-tree=$PROJECT_DIR/CustomDevices --git-dir=$PROJECT_DIR/CustomDevices/.git pull origin main --depth 100")
