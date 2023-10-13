517385bc260f6ff896521e8971adbe4aaec21145
elral <3263285+elral@users.noreply.github.com>
2023-10-13 08:22:08 +0200

    max. numbers of devices not required anymore

--- _Boards/Atmel/Board_Mega/MFBoards.h
+++ _Boards/Atmel/Board_Mega/MFBoards.h
@@ -36,40 +36,6 @@
 #define MF_DIGIN_MUX_SUPPORT 1
 #endif
 
-#ifndef MAX_OUTPUTS
-#define MAX_OUTPUTS         40
-#endif
-#ifndef MAX_BUTTONS
-#define MAX_BUTTONS         68
-#endif
-#ifndef MAX_LEDSEGMENTS
-#define MAX_LEDSEGMENTS     4
-#endif
-#ifndef MAX_ENCODERS
-#define MAX_ENCODERS        20
-#endif
-#ifndef MAX_STEPPERS
-#define MAX_STEPPERS        10
-#endif
-#ifndef MAX_MFSERVOS
-#define MAX_MFSERVOS        10
-#endif
-#ifndef MAX_MFLCD_I2C
-#define MAX_MFLCD_I2C       2
-#endif
-#ifndef MAX_ANALOG_INPUTS
-#define MAX_ANALOG_INPUTS   16
-#endif
-#ifndef MAX_OUTPUT_SHIFTERS
-#define MAX_OUTPUT_SHIFTERS 4
-#endif
-#ifndef MAX_INPUT_SHIFTERS
-#define MAX_INPUT_SHIFTERS  4
-#endif
-#ifndef MAX_DIGIN_MUX
-#define MAX_DIGIN_MUX       4
-#endif
-
 #ifndef MOBIFLIGHT_TYPE
 #define MOBIFLIGHT_TYPE     "MobiFlight Mega"
 #endif
--- _Boards/Atmel/Board_Uno/MFBoards.h
+++ _Boards/Atmel/Board_Uno/MFBoards.h
@@ -36,40 +36,6 @@
 #define MF_DIGIN_MUX_SUPPORT 1
 #endif
 
-#ifndef MAX_OUTPUTS
-#define MAX_OUTPUTS         18
-#endif
-#ifndef MAX_BUTTONS
-#define MAX_BUTTONS         18
-#endif
-#ifndef MAX_LEDSEGMENTS
-#define MAX_LEDSEGMENTS     4
-#endif
-#ifndef MAX_ENCODERS
-#define MAX_ENCODERS        9
-#endif
-#ifndef MAX_STEPPERS
-#define MAX_STEPPERS        2
-#endif
-#ifndef MAX_MFSERVOS
-#define MAX_MFSERVOS        2
-#endif
-#ifndef MAX_MFLCD_I2C
-#define MAX_MFLCD_I2C       2
-#endif
-#ifndef MAX_ANALOG_INPUTS
-#define MAX_ANALOG_INPUTS   6
-#endif
-#ifndef MAX_OUTPUT_SHIFTERS
-#define MAX_OUTPUT_SHIFTERS 2
-#endif
-#ifndef MAX_INPUT_SHIFTERS
-#define MAX_INPUT_SHIFTERS  2
-#endif
-#ifndef MAX_DIGIN_MUX
-#define MAX_DIGIN_MUX       3
-#endif
-
 #ifndef MOBIFLIGHT_TYPE
 #define MOBIFLIGHT_TYPE     "MobiFlight Uno"
 #endif
