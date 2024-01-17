e5ffd8e3caf9e59c07eaae0913d70933fb687660
elral <3263285+elral@users.noreply.github.com>
2024-01-17 18:23:02 +0100

    create zip folder, add reset and json files

--- get_version.py
+++ get_version.py
@@ -1,5 +1,6 @@
 Import("env")
-import os
+import os, zipfile, shutil
+from pathlib import Path
 
 # Get the version number from the build environment.
 firmware_version = os.environ.get('VERSION', "")
@@ -30,3 +31,63 @@ env.Append(CPPDEFINES=[
 
 # Set the output filename to the name of the board and the version
 env.Replace(PROGNAME=f'{env["PIOENV"]}_{firmware_version.replace(".", "_")}')
+
+
+zip_file_name = 'Mobiflight-Connector'
+build_path = './_build'
+build_path_fw = build_path + '/firmware'
+build_path_json = build_path + '/Boards'
+distrubution_path = './_dist'
+source_folders = ['./_Boards']
+
+def copy_fw_files (source, target, env):
+    fw_file_name=str(target[0])
+
+    if os.path.exists(build_path_fw) == False:
+        os.makedirs(build_path_fw)
+    if os.path.exists(build_path_json) == False:
+        os.makedirs(build_path_json)
+
+    if fw_file_name[-3:] == "bin":
+        fw_file_name=fw_file_name[0:-3] + "uf2"
+    
+    shutil.copy(fw_file_name, build_path_fw)
+
+    file_extension = '.hex'
+    copy_files_by_extension(source_folders, build_path_fw, file_extension)
+    file_extension = '.uf2'
+    copy_files_by_extension(source_folders, build_path_fw, file_extension)
+    file_extension = '.json'
+    copy_files_by_extension(source_folders, build_path_json, file_extension)
+
+    createCommunityZipFile(source, target, env)
+
+def copy_files_by_extension(source_folders, target_folder, file_extension):
+    for source_folder in source_folders:
+        for root, dirs, files in os.walk(source_folder):
+            for file in files:
+                if file.endswith(file_extension):
+                    source_path = os.path.join(root, file)
+                    target_path = os.path.join(target_folder, file)
+                    shutil.copy2(source_path, target_path)
+
+def createCommunityZipFile(source, target, env):
+    original_folder_path = build_path
+    zip_file_path = distrubution_path + '/' + zip_file_name + '_' + firmware_version + '.zip'
+    new_folder_in_zip = zip_file_name
+    createZIP(original_folder_path, zip_file_path, new_folder_in_zip)
+
+def createZIP(original_folder_path, zip_file_path, new_folder_name):
+    if os.path.exists(distrubution_path) == False:
+        os.mkdir(distrubution_path)
+    with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+        for root, dirs, files in os.walk(original_folder_path):
+            for file in files:
+                # Create a new path in the ZIP file
+                new_path = os.path.join(new_folder_name, os.path.relpath(os.path.join(root, file), original_folder_path))
+                # Add the file to the ZIP file
+                zipf.write(os.path.join(root, file), new_path)
+
+
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", copy_fw_files)
+env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", copy_fw_files)
