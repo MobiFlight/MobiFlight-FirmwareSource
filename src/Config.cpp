c56df66368ece00c580ab026ff49d10247268c2d
elral <3263285+elral@users.noreply.github.com>
2024-01-17 12:59:23 +0100

    send kDebug instead of kInfo on generating serial

--- src/Config.cpp
+++ src/Config.cpp
@@ -590,7 +590,7 @@ void generateRandomSerial()
         randomSerial >>= 4;
     }
     MFeeprom.write_block(MEM_OFFSET_SERIAL, serial, MEM_LEN_SERIAL);
-    cmdMessenger.sendCmd(kInfo, F("Serial number generated"));
+    cmdMessenger.sendCmd(kDebug, F("Serial number generated"));
 }
 
 #if defined(ARDUINO_ARCH_RP2040)
