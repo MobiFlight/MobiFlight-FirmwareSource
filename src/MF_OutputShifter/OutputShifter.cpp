ce33ba943cf5ce5a92339571ced681aeddbe11e9
elral <3263285+elral@users.noreply.github.com>
2026-07-16 08:28:40 +0200

    fixed wrong order of received oins, add explanation of how to send command

--- src/MF_OutputShifter/OutputShifter.cpp
+++ src/MF_OutputShifter/OutputShifter.cpp
@@ -63,15 +63,25 @@ namespace OutputShifter
     5) bitmask of which pins to be set/unset
     First byte of bit mask must be MSB
     e.g. 27,0,1,3,[0x12345678][0x87654321][0x12348765];
+
+    in cpp it would be:
+    cmdMessenger.sendCmdStart (kSetShiftRegisterPins);
+    cmdMessenger.sendCmdArg<uint8_t>((uint8_t)module);
+    cmdMessenger.sendCmdArg<uint8_t>((uint8_t)value);
+    cmdMessenger.sendCmdArg<uint8_t>((uint8_t)number_of_submodules);
+    for (uint8_t i = number_of_submodules - 1; i != 0; i--) {
+        cmdMessenger.sendCmdBinArg<uint8_t>((uint8_t)value[i]);
+    }
+    cmdMessenger.sendCmdEnd ();
     */
     void OnSet()
     {
-        int     module              = cmdMessenger.readInt16Arg(); // which chain of shifter
-        int     value               = cmdMessenger.readInt16Arg(); // set or reset pin
-        int     _moduleCount        = cmdMessenger.readInt16Arg(); // number of 8bit Register
-        uint8_t _pins[_moduleCount] = {0};                         // pin mask
+        int     module                      = cmdMessenger.readInt16Arg(); // which chain of shifter
+        int     value                       = cmdMessenger.readInt16Arg(); // set or reset pin
+        int     number_of_submodules        = cmdMessenger.readInt16Arg(); // number of 8bit Register
+        uint8_t _pins[number_of_submodules] = {0};                         // pin mask
 
-        for (uint8_t i = 0; i < _moduleCount; i++) {
+        for (uint8_t i = number_of_submodules - 1; i != 0; i--) {
             _pins[i] = cmdMessenger.readBinArg<uint8_t>(); // read in data
         }
         outputShifter[module].setPins(_pins, value); // call set function of class
